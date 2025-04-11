41184566f880f1de08f5fb3a46020ea0e4769037
Andrei <ierdss@proton.me>
2025-04-11 09:21:45 +0800

    feat: print tasks

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Purpose
 
 I am just too lazy to create multiple tasks on Todoist one by one.
+
+# Example Usage
+
+```js
+{
+  "section": "37",
+  "start": "322",
+  "end": "328"
+}
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+const questions = [
+  {
+    type: "input",
+    name: "section",
+    message: "What section are you on?",
+  },
+  {
+    type: "input",
+    name: "start",
+    message: "What is the start of the range?",
+  },
+  {
+    type: "input",
+    name: "end",
+    message: "What is the end of the range?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, "  "));
+  for (let i = answers.start; i <= answers.end; i++) {
+    console.log("Complete Section " + answers.section + " " + i);
+  }
+});
